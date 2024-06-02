00bcb83177e82d1a555a75052a51a923b20ff483
FilipHa5 <filipha5@wp.pl>
2024-06-02 14:41:52 +0200

    Added transforms

---
+++ transforms.py
@@ -0,0 +1,108 @@
+from torch.utils.data import Dataset, DataLoader, random_split
+from torchvision import transforms, utils
+import torch
+import os
+import numpy as np
+import random as r
+
+class VMPData(Dataset):
+  def __init__(self, path, transform=None):
+
+    self.fileslist = os.listdir(path)
+    self.files = [np.load(os.path.join(path, file), allow_pickle = True) for file in self.fileslist]
+    self.data = []
+    self.filenames = []
+    self.labels = []
+    self.transform = transform
+    for index, filename_r_x in enumerate(self.files):
+        for element in filename_r_x:
+            self.labels.append(torch.tensor(np.array(index), dtype = torch.long))
+            self.filenames.append(element[0])
+            self.data.append([element[1], element[2]])
+
+  def __getitem__(self, index):
+    if self.transform:
+      return [self.transform(self.data[index]), self.labels[index]]
+    return [self.transform(self.data[index]), self.labels[index]]
+
+  def __len__(self):
+    return len(self.data)
+
+class GetChannels(object):
+  def __init__(self, channels):
+    self.channels = channels
+
+  def __call__(self, r_x_data):
+    r_reads = r_x_data[0][self.channels]
+    x_reads = r_x_data[1][self.channels]
+    return [r_reads, x_reads]
+
+class RollAllChannels(object):
+  def __call__(self, r_x_reads):
+    r_x_reads = r_x_reads
+    r =  r_x_reads[0]
+    x = r_x_reads[1]
+
+    if np.random.rand() > .5:
+      return r_x_reads
+
+    self._calc_to_move(r[0])
+
+    r = [np.array(np.roll(sig, self.to_move)) for sig in r]
+    x = [np.array(np.roll(sig, self.to_move)) for sig in x]
+    return [r, x]
+
+  def _calc_to_move(self, signal):
+    self.padding_size = self._calc_padding(signal)
+    self.to_move = self._draw_number_to_move()
+
+  def _draw_number_to_move(self):
+    to_move = r.randint(0, self.padding_size)
+    return to_move
+
+  def _calc_padding(self, signal):
+    start, stop = self._get_first_last_nonzero_index(signal)
+    values_len = len(range(start, stop))
+    pad_len = len(signal) - values_len
+    return pad_len
+
+  def _get_first_last_nonzero_index(self, array):
+    nonzero_indxs = np.nonzero(array)
+    first = nonzero_indxs[0][0]
+    last = nonzero_indxs[0][-1]
+    return first, last
+
+class StackSignals(object):
+  def __call__(self, data):
+    r_reads = np.hstack(data[0])
+    x_reads = np.hstack(data[1])
+    joined_signals = [r_reads, x_reads]
+    return np.hstack(joined_signals)
+
+class ReduceSamplingRate():
+  def __init__(self, reduce_factor):
+    self.reduce_factor = reduce_factor
+
+  def __call__(self, data):
+    signal = data[::self.reduce_factor]
+    return signal
+
+class Absolute(object):
+  def __call__(self, stacked_signal):
+    return np.absolute(stacked_signal)
+
+class Scale(object):
+  def __init__(self, scale):
+    self.scale = scale
+  def __call__(self, stacked_signal):
+    return stacked_signal * self.scale
+
+class AddOffset(object):
+  def __init__(self, offset):
+    self.offset = offset
+  def __call__(self, stacked_signal):
+    return [sample + self.offset for sample in stacked_signal]
+
+class ToTensor(object):
+  def __call__(self, signal):
+    return torch.tensor(signal, dtype = torch.float32)
