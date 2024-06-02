37fe4252f3ae1a1bd8d8963da7b1ab394aa21c97
FilipHa5 <filipha5@wp.pl>
2024-06-02 20:41:20 +0200

    Removed reduntant line of code, fixed functionality to remove suffix

--- mapping.py
+++ mapping.py
@@ -2,6 +2,5 @@ import os
 
 def get_class_to_filename_map(path_to_files: str) -> dict:
     fileslist = os.listdir(path_to_files)
-    class_names = [filename.rstrip(".npy") for filename in fileslist]
-    class_to_name_map = {index: filename.removeprefix(".npy") for index, filename in enumerate(fileslist)}
+    class_to_name_map = {index: filename.removesuffix(".npy") for index, filename in enumerate(fileslist)}
     return class_to_name_map
